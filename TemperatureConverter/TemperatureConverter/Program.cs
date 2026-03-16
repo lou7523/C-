ff3c4690b81bde384b8ef69709e92616ee07f115
Lourenco Francisco <ldrsf23042009@gmail.com>
2026-03-16 22:38:37 +0000

    Add files via upload

---
+++ TemperatureConverter/TemperatureConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to my temperature converter (Cº -> Fº)");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadKey();
+            Console.Write("Choose your temperature (Cº): ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double f = (c * 9 / 5) + 32;
+            
+            Console.WriteLine(c + "º Celsius will be " + f + "º Fahrenheit");
+
+            Console.ReadKey();
+        }
+    }
+}
