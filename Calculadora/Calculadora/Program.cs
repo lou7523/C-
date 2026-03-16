6825b4538df63cd053a39d2186b88f78696ff943
Lourenco Francisco <ldrsf23042009@gmail.com>
2026-03-16 22:27:09 +0000

    Adde Calculadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int multiply = 1;
+            double division = 1;
+
+            Console.WriteLine("Choose the total of numbers: ");
+            int qnt =  Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < qnt; i++)
+            {
+                Console.WriteLine("Insert a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+                multiply *= number;
+                division /= number;
+
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Multiply: {multiply}");
+            Console.WriteLine($"Division: {division}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
